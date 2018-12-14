6a3622f619d8efebc444ef3671a1a345e4198201
robbix1206 <robbix1206@gmail.com>
2018-12-14 03:16:15 +0100

    Add Graphics to cars

--- src/Patient/DiseaseCard.js
+++ src/Patient/DiseaseCard.js
@@ -6,8 +6,43 @@ import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Chart from 'chart.js';
 
 class DiseaseCard extends PureComponent {
+  setRef = ref => this.ref = ref;
+
+  componentDidMount() {
+    let ctx = this.ref.getContext('2d');
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: [
+          '20/10',
+          '21/10',
+          '22/10',
+          '23/10',
+          '24/10',
+          '25/10',
+          '26/10',
+        ],
+        datasets: [{
+          label: 'Glycemie',
+          data: [
+            20,
+            14,
+            15,
+            12,
+            19,
+            17,
+            14,
+          ]
+        }],
+      },
+      options: {
+      }
+    });
+  }
+
   render() {
     const {
       disease,
@@ -19,8 +54,10 @@ class DiseaseCard extends PureComponent {
           <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
             <Typography variant="h6">{disease}</Typography>
           </ExpansionPanelSummary>
-          {children}
-          <ExpansionPanelDetails></ExpansionPanelDetails>
+          <ExpansionPanelDetails>
+            {children}
+            <div><canvas ref={this.setRef} width="400" height="400"></canvas></div>
+          </ExpansionPanelDetails>
         </ExpansionPanel>
       </Card>
     );
