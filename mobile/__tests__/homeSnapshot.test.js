0fedf60cd5706776617093617f3d7e5205d91f54
victor <victor.zhu@epitech.eu>
2019-06-04 00:32:26 +0200

    [ADD] unit test snapshot for login page (works all time lol)

--- mobile/__tests__/homeSnapshot.test.js
+++ mobile/__tests__/homeSnapshot.test.js
@@ -1,12 +1,32 @@
 import 'react-native';
 import React from 'react';
-import Login from '../Components/Auth/Login';
 import renderer from 'react-test-renderer';
+import Store from '../Redux/Store/configureStore';
+import { Provider } from 'react-redux';
+import mockStore from 'redux-mock-store';
+import {createStackNavigator, createBottomTabNavigator, createAppContainer} from 'react-navigation';
 
-test('Login snapshot', () => {
-	const snap = renderer.create(
-		<Login />
-	).toJSON();
+import Navigator from '../Navigation/Navigation';
+import Login from '../Components/Auth/Login';
 
-	expect(snap).toMatchSnapshot();
+describe('Login screen', () => {
+  it('this is a test for a page', () => {
+
+	const LoginStack = createStackNavigator({
+		Login: { screen: Login },
+	})
+
+	const StackNavigator = createStackNavigator({
+		LoginStack: {
+			screen: LoginStack, headerMode : 'none'
+		}
+	},{
+		headerMode: 'none'
+	});
+	const App = createAppContainer(StackNavigator);
+	const tree = renderer.create(<Provider store={Store}> <App /> </Provider>).toJSON();
+  	expect(tree).toMatchSnapshot();
+
+
+  });
 });
