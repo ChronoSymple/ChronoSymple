bb352f097fad044fe9858fd824485feb887f7a0c
carl2g <degentilecarl@gmail.com>
2020-04-05 14:09:17 +0200

    share note to all doctors

--- mobile/API/APIModule.js
+++ mobile/API/APIModule.js
@@ -2,6 +2,7 @@ import { stringify } from "qs";
 
 //const baseUrl = 'http://192.168.0.11:3000'
 const baseUrl = 'https://docapp-prod.herokuapp.com'
+//const baseUrl = 'https://docapp-preprod.herokuapp.com'
 
 export function APIAddModule (token, id) {
 	return fetch(baseUrl + '/api/patients/general_units/' + id +"/add_unit" , {
@@ -95,6 +96,33 @@ export function APIEditPatientNotes(token, myTab, idNote) {
 export function APIRemoveUnit(token, id) {
 	return fetch(baseUrl + '/api/patients/units/' + id, {
 		method: 'DELETE',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': token,
+		}
+	})
+	.then((response) => response)
+	.catch((error) => error)
+}
+
+export function APIShareNote(token, module_id, note_ids, doctor_ids) {
+		return fetch(baseUrl + '/api/patients/units/' + module_id + '/share_notes', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': token,
+		}, body: JSON.stringify({
+			doctor_ids: doctor_ids,
+			note_ids: note_ids
+		})
+	})
+	.then((response) => response)
+	.catch((error) => error)
+}
+
+export function APIgetDoctorsOfModule(token, id) {
+	return fetch(baseUrl + '/api/patients/units/' + id + '/doctors', {
+		method: 'GET',
 		headers: {
 			'Content-Type': 'application/json',
 			'Authorization': token,
