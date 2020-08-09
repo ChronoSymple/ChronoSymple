30fec484e2c373679baba58fbddfa36f49b3f1ef
Hinara Turevel <hinara.turevel@gmail.com>
2020-08-09 13:31:54 +0200

    Add nous contacter

--- web/src/Components/LoginCard.js
+++ web/src/Components/LoginCard.js
@@ -5,6 +5,7 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
 import Alert from '@material-ui/lab/Alert';
 import { withStyles } from '@material-ui/core';
 
@@ -30,49 +31,59 @@ const LoginCard = ({
   openRegister,
   error
 }) => (
-  <Card className={classes.loginCard}>
-    <CardContent>
-      <Typography variant="h5">Connexion</Typography>
-      {error && <Alert severity='error'>{error}</Alert>}
-      <form onSubmit={login}>
-        <TextField
-          label="Email"
-          value={email}
-          onChange={setEmail}
-          margin="normal"
-          fullWidth
-          type="email"
-        />
-        <TextField
-          label="Mot de passe"
-          value={password}
-          onChange={setPassword}
-          margin="normal"
-          type="password"
-          fullWidth
-        />
-        <div style={{display: 'flex'}}>
-          <div style={{flex: 1}}/>
-          <Button
-            className={classes.loginButton}
-            color="primary"
-            onClick={openRegister}
-          >
-            {'S\'inscrire'}
-          </Button>
-          <Button
-            className={classes.loginButton}
-            type="submit"
-            color="primary"
-            variant="contained"
-            onClick={login}
-          >
-            Se connecter
-          </Button>
-        </div>
-      </form>
-    </CardContent>
-  </Card>
+  <div>
+    <AppBar>
+      <div style={{display: 'flex'}}>
+        <div style={{flex: 1}} />
+        <a href='https://eip.epitech.eu/2021/chronosymple/'>
+          <Button style={{margin: 6}} color="default" variant="outlined" >Nous Contacter</Button>
+        </a>
+      </div>
+    </AppBar>
+    <Card className={classes.loginCard}>
+      <CardContent>
+        <Typography variant="h5">Connexion</Typography>
+        {error && <Alert severity='error'>{error}</Alert>}
+        <form onSubmit={login}>
+          <TextField
+            label="Email"
+            value={email}
+            onChange={setEmail}
+            margin="normal"
+            fullWidth
+            type="email"
+          />
+          <TextField
+            label="Mot de passe"
+            value={password}
+            onChange={setPassword}
+            margin="normal"
+            type="password"
+            fullWidth
+          />
+          <div style={{display: 'flex'}}>
+            <div style={{flex: 1}}/>
+            <Button
+              className={classes.loginButton}
+              color="primary"
+              onClick={openRegister}
+            >
+              {'S\'inscrire'}
+            </Button>
+            <Button
+              className={classes.loginButton}
+              type="submit"
+              color="primary"
+              variant="contained"
+              onClick={login}
+            >
+              Se connecter
+            </Button>
+          </div>
+        </form>
+      </CardContent>
+    </Card>
+  </div>
 );
 
 LoginCard.propTypes = {
