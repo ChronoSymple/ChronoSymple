36b74156c9872fb374981d9dd68a78d930549a1a
Robin Milas <robin.milas@ysocorp.com>
2020-11-22 17:01:06 +0100

    Fix graph

---
+++ web/src/Components/Patient/DiseaseCard/DiabeteGraph.js
@@ -0,0 +1,184 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import Chart, { helpers } from 'chart.js';
+import ChartAnnotation from 'chartjs-plugin-annotation';
+
+class DiabeteGraph extends PureComponent {
+  setRef = ref => {
+    this.ref = ref;
+    this.ctx = this.ref.getContext('2d');
+  }
+  setDivRef = ref => this.divref = ref;
+  dateToDateOfTheDay = date => {
+    const divider = 1000 * 60 * 60 * 24;
+    const time = date.getTime() + date.getTimezoneOffset() * 60 * 1000;
+    const remaining = time % divider;
+    return new Date(time + ((remaining === 0) ? 0 : divider - remaining));
+  }
+  nextDay = date => new Date(date.getTime() + 1000 * 60 * 60 * 24);
+  convertToGraphData = data => {
+    const meals = {
+      'Petit déjeuner': 'rgb(255, 99, 132)',
+      'Repas': 'rgb(54, 162, 235)',
+      'Gouter': 'rgb(255, 205, 86)',
+      'Dîner': 'rgb(75, 192, 192)',
+    };
+    data = data.map(e => e.note).map(({date, ...e}) => ({...e, date: new Date(date)}));
+    data = data.filter(e => e !== undefined && e.date !== undefined && e.data !== undefined && e.data.bloodGlucose !== undefined);
+    if (data.length === 0) {
+      return;
+    }
+    const biggestDate = data.map(e => e.date).reduce((acc, e) => acc === null ? e : ((acc > e) ? acc : e), null);
+    const smallestDate = data.map(e => e.date).reduce((acc, e) => acc === null ? e : ((acc < e) ? acc : e), null);
+    data = data.reduce((acc, e) => {
+      const wichLunch = e.data.wichLunch;
+      if (Object.keys(meals).includes(wichLunch) === false) {
+        return acc;
+      }
+      return ({ ...acc, [wichLunch]: [...acc[wichLunch], e] });
+    }, Object.keys(meals).reduce((acc, key) => ({ ...acc, [key]: [] }), {}));
+    Object.keys(data).map(key => data[key].sort((a, b) => a.date.getTime() - b.date.getTime()));
+    const tmp = Object.keys(data).reduce((acc, key) => {
+      const value = data[key];
+      let newValue = value.reduce(({lastDate, arr}, e) => {
+        while (this.dateToDateOfTheDay(e.date) > this.dateToDateOfTheDay(lastDate)) {
+          arr = [...arr, null];
+          lastDate = this.nextDay(lastDate);
+        }
+        arr = [...arr, e];
+        lastDate = this.nextDay(lastDate);
+        return {lastDate, arr};
+      }, { lastDate:smallestDate, arr:[]}).arr;
+      let lastDate = (newValue.length === 0) ? smallestDate : this.nextDay(newValue[newValue.length - 1].date);
+      while (this.dateToDateOfTheDay(lastDate) <= this.dateToDateOfTheDay(biggestDate)) {
+        newValue = [...newValue, null];
+        lastDate = this.nextDay(lastDate);
+      }
+      newValue = newValue.map(e => e === null ? NaN : e.data.bloodGlucose);
+      return {...acc, [key]: newValue};
+    }, {});
+    data = Object.keys(meals).reduce((acc, key) => {
+      const value = tmp[key];
+      const newElem = {
+        label: key,
+        backgroundColor: helpers.color(meals[key]).alpha(0.5).rgbString(),
+        borderColor: meals[key],
+        borderWidth: 1,
+        data: value
+      };
+      return ([...acc, newElem]);
+    }, []);
+    let labels = [];
+    let lastDate = smallestDate;
+    while (this.dateToDateOfTheDay(lastDate) <= this.dateToDateOfTheDay(biggestDate)) {
+      labels = [...labels, `${lastDate.getDate()}/${lastDate.getMonth()}/${lastDate.getFullYear()}`];
+      lastDate = this.nextDay(lastDate);
+    }
+    const graphData = {
+      datasets: data,
+      labels,
+    };
+    const res = {
+      type: 'bar',
+      plugins: [ChartAnnotation],
+      data: graphData,
+      options: {
+        annotation: {
+          annotations: [],
+          drawTime: 'afterDraw'
+        },
+        maintainAspectRatio: false,
+        responsive: true,
+        legend: {
+          position: 'top',
+        }, 
+        title: {
+          display: true,
+          text: 'Glycémie'
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      },
+    };
+    if (this.props.min >= 0) {
+      const obj = {
+        type: 'line',
+        mode: 'horizontal',
+        scaleID: 'y-axis-0',
+        value: this.props.min,
+        borderColor: 'tomato',
+        borderWidth: 1,
+        label: {
+          backgroundColor: 'red',
+          content: 'Hypoglycémie',
+          position: 'left',
+          enabled: true
+        }
+      };
+      res.options.annotation.annotations = [...res.options.annotation.annotations, obj];
+    }
+    if (this.props.max >= 0) {
+      const obj = {
+        type: 'line',
+        mode: 'horizontal',
+        scaleID: 'y-axis-0',
+        value: this.props.max,
+        borderColor: 'tomato',
+        borderWidth: 1,
+        label: {
+          backgroundColor: 'red',
+          content: 'Hyperglycémie',
+          position: 'left',
+          enabled: true
+        }
+      };
+      res.options.annotation.annotations = [...res.options.annotation.annotations, obj];
+    }
+    return res;
+  }
+  componentDidMount() {
+    this.ctx = this.ref.getContext('2d');
+    this.ctx.canvas.height = this.divref.clientHeight;
+    this.ctx.canvas.width = this.divref.clientWidth;
+    const config = this.convertToGraphData(this.props.data);
+    this.chart = new Chart(this.ctx, config);
+    
+    //window.addEventListener('resize', this.onResize);
+  }
+  componentDidUpdate() {
+    if(this.chart) {
+      this.chart.destroy();
+      this.ctx = this.ref.getContext('2d');
+      this.ctx.canvas.height = this.divref.clientHeight;
+      this.ctx.canvas.width = this.divref.clientWidth;
+      const config = this.convertToGraphData(this.props.data);
+      this.chart = new Chart(this.ctx, config);
+    }
+  }
+  componentWillUnmount() {
+    //window.removeEventListener('resize', this.onResize);
+    if (this.chart) {
+      this.chart.destroy();
+    }
+  }
+  render = () =>
+    <div ref={this.setDivRef} style={{height: 500, width: '100%', overflow: 'none'}}>
+      <canvas ref={this.setRef}/>
+    </div>
+}
+
+DiabeteGraph.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape({
+    date: PropTypes.instanceOf(Date),
+    data: PropTypes.number
+  })),
+  min: PropTypes.number.isRequired,
+  max: PropTypes.number.isRequired,
+};
+
+export default DiabeteGraph;
--- web/src/Components/Patient/DiseaseCard/Diabetes.js
+++ web/src/Components/Patient/DiseaseCard/Diabetes.js
@@ -1,196 +1,22 @@
 import React, { PureComponent } from 'react';
-import Chart, { helpers } from 'chart.js';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import PropTypes from 'prop-types';
 import Api from '../../../Api';
-import ChartAnnotation from 'chartjs-plugin-annotation';
-//import { createPalette } from '@material-ui/core';
-//import * as ChartAnnotation from 'chartjs-plugin-annotation';
-
+import DiabeteGraph from './DiabeteGraph';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import Typography from '@material-ui/core/Typography';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 class Diabetes extends PureComponent {
-  setRef = ref => this.ref = ref;
-  setDivRef = ref => this.divref = ref;
-
   state = {
     min: -1,
     max: -1,
+    defMin: -1,
+    defMax: -1,
   };
 
-  dateToDateOfTheDay = date => {
-    const divider = 1000 * 60 * 60 * 24;
-    const time = date.getTime() + date.getTimezoneOffset() * 60 * 1000;
-    const remaining = time % divider;
-    return new Date(time + ((remaining === 0) ? 0 : divider - remaining));
-  }
-  nextDay = date => new Date(date.getTime() + 1000 * 60 * 60 * 24);
-  convertToGraphData = data => {
-    const meals = {
-      'Petit déjeuner': 'rgb(255, 99, 132)',
-      'Repas': 'rgb(54, 162, 235)',
-      'Gouter': 'rgb(255, 205, 86)',
-      'Diner': 'rgb(75, 192, 192)',
-    };
-    data = data.map(e => e.note).map(({date, ...e}) => ({...e, date: new Date(date)}));
-    data = data.filter(e => e !== undefined && e.date !== undefined && e.data !== undefined && e.data.bloodGlucose !== undefined);
-    if (data.length === 0) {
-      return;
-    }
-    const biggestDate = data.map(e => e.date).reduce((acc, e) => acc === null ? e : ((acc > e) ? acc : e), null);
-    const smallestDate = data.map(e => e.date).reduce((acc, e) => acc === null ? e : ((acc < e) ? acc : e), null);
-    data = data.reduce((acc, e) => {
-      const wichLunch = e.data.wichLunch;
-      return ({ ...acc, [wichLunch]: [...acc[wichLunch], e] });
-    }, Object.keys(meals).reduce((acc, key) => ({ ...acc, [key]: [] }), {}));
-    Object.keys(data).map(key => data[key].sort((a, b) => a.date.getTime() - b.date.getTime()));
-
-    const tmp = Object.keys(data).reduce((acc, key) => {
-      const value = data[key];
-      let newValue = value.reduce(({lastDate, arr}, e) => {
-        while (this.dateToDateOfTheDay(e.date) > this.dateToDateOfTheDay(lastDate)) {
-          arr = [...arr, null];
-          lastDate = this.nextDay(lastDate);
-        }
-        arr = [...arr, e];
-        lastDate = this.nextDay(lastDate);
-        return {lastDate, arr};
-      }, { lastDate:smallestDate, arr:[]}).arr;
-      let lastDate = (newValue.length === 0) ? smallestDate : this.nextDay(newValue[newValue.length - 1].date);
-      while (this.dateToDateOfTheDay(lastDate) <= this.dateToDateOfTheDay(biggestDate)) {
-        newValue = [...newValue, null];
-        lastDate = this.nextDay(lastDate);
-      }
-      newValue = newValue.map(e => e === null ? NaN : e.data.bloodGlucose);
-      return {...acc, [key]: newValue};
-    }, {});
-    data = Object.keys(meals).reduce((acc, key) => {
-      const value = tmp[key];
-      const newElem = {
-        label: key,
-        backgroundColor: helpers.color(meals[key]).alpha(0.5).rgbString(),
-        borderColor: meals[key],
-        borderWidth: 1,
-        data: value
-      };
-      return ([...acc, newElem]);
-    }, []);
-    let labels = [];
-    let lastDate = smallestDate;
-    while (this.dateToDateOfTheDay(lastDate) <= this.dateToDateOfTheDay(biggestDate)) {
-      labels = [...labels, `${lastDate.getDate()}/${lastDate.getMonth()}/${lastDate.getFullYear()}`];
-      lastDate = this.nextDay(lastDate);
-    }
-    const graphData = {
-      datasets: data,
-      labels,
-    };
-    
-    const res = {
-      type: 'bar',
-      plugins: [ChartAnnotation],
-      data: graphData,
-      options: {
-        annotation: {
-          annotations: [],
-          drawTime: 'afterDraw'
-        },
-        maintainAspectRatio: false,
-        responsive: true,
-        legend: {
-          position: 'top',
-        }, 
-        title: {
-          display: true,
-          text: 'Glycémie'
-        },
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      },
-      
-    };
-    if (this.state.min >= 0) {
-      const obj = {
-        type: 'line',
-        mode: 'horizontal',
-        scaleID: 'y-axis-0',
-        value: this.state.min,
-        borderColor: 'tomato',
-        borderWidth: 1,
-        label: {
-          backgroundColor: 'red',
-          content: 'Hypoglycémie',
-          position: 'left',
-          enabled: true
-        }
-      };
-      res.options.annotation.annotations = [...res.options.annotation.annotations, obj];
-    }
-    if (this.state.max >= 0) {
-      const obj = {
-        type: 'line',
-        mode: 'horizontal',
-        scaleID: 'y-axis-0',
-        value: this.state.max,
-        borderColor: 'tomato',
-        borderWidth: 1,
-        label: {
-          backgroundColor: 'red',
-          content: 'Hyperglycémie',
-          position: 'left',
-          enabled: true
-        }
-      };
-      res.options.annotation.annotations = [...res.options.annotation.annotations, obj];
-    }
-    return res;
-  }
-
-  /*
-  {
-        type: 'line',
-        mode: 'horizontal',
-        scaleID: 'y-axis-0',
-        value: '60',
-        borderColor: 'tomato',
-        borderWidth: 1,
-        label: {
-          backgroundColor: 'red',
-          content: 'Hypoglycémie',
-          enabled: table.some(e => e.data < 60),
-          position : 'left'
-        }
-      }, {
-        type: 'line',
-        mode: 'horizontal',
-        scaleID: 'y-axis-0',
-        value: '110',
-        borderColor: 'tomato',
-        borderWidth: 1,
-        label: {
-          backgroundColor: 'red',
-          content: 'Hyperglycémie à jeun',
-          enabled: true,
-          position : 'left'
-        }
-      }, {
-        type: 'line',
-        mode: 'horizontal',
-        scaleID: 'y-axis-0',
-        value: '140',
-        borderColor: 'tomato',
-        borderWidth: 1,
-        label: {
-          backgroundColor: 'red',
-          content: 'Hyperglycémie après repas',
-          enabled: table.some(e => e.data > 140),
-          position : 'left'
-        }
-      }*/
 
   onResize = () => {
     //this.ctx.canvas.height = window.innerHeight * 0.7;
@@ -201,7 +27,7 @@ class Diabetes extends PureComponent {
   getLimits = async() => {
     try {
       const req = await Api.getLimits(localStorage.getItem('myToken'), this.props.unitID);
-      const insuline = req['Insuline'];
+      const insuline = req['bloodGlucose'];
       if (insuline !== null) {
         let {'min_limit': min, 'max_limit': max} = insuline;
         if (min === undefined) {
@@ -211,9 +37,6 @@ class Diabetes extends PureComponent {
           max = -1;
         }
         this.setState({min, max});
-        const oldChart = this.chart;
-        this.chart = new Chart(this.ctx, this.convertToGraphData(this.props.data));
-        oldChart.destroy();
       }
     } catch (e) {
       // Because of 404
@@ -221,30 +44,14 @@ class Diabetes extends PureComponent {
   }
 
   setLimits = async() => {
-    const oldChart = this.chart;
-    this.chart = new Chart(this.ctx, this.convertToGraphData(this.props.data));
-    oldChart.destroy();
+    const min = Number(this.state.min);
+    const max = Number(this.state.max);
     await Api.setLimits(localStorage.getItem('myToken'), this.props.unitID, {
       'fields_limits': {
-        'Insuline': {'min_limit': this.state.min, 'max_limit': this.state.max}
+        'bloodGlucose': {'min_limit': min, 'max_limit': max}
       }
     });
-  }
-
-  componentDidMount() {
-    this.ctx = this.ref.getContext('2d');
-    //this.ctx.canvas.height = window.innerHeight * 0.7;
-    this.ctx.canvas.width = this.divref.clientWidth;
-    this.chart = new Chart(this.ctx, this.convertToGraphData(this.props.data));
-    this.getLimits(this.props.unitID);
-    
-    //window.addEventListener('resize', this.onResize);
-  }
-  componentWillUnmount() {
-    //window.removeEventListener('resize', this.onResize);
-    if (this.chart) {
-      this.chart.destroy();
-    }
+    this.setState({defMin: min, defMax: max});
   }
 
   setMin = e => {
@@ -256,15 +63,13 @@ class Diabetes extends PureComponent {
     const max = e.target.value;
     this.setState({max});
   }
-
-  changeStep = (e, v) => this.setState({step: v}, this.generateProbsGraph);
-
+  componentDidMount() {
+    this.getLimits(this.props.unitID);
+  }
   render() {
     return (
       <div style={{width: '100%'}}>
-        <div ref={this.setDivRef} style={{height: 500, width: '100%', overflow: 'none'}}>
-          <canvas ref={this.setRef}/>
-        </div>
+        <DiabeteGraph min={this.state.defMin} max={this.state.defMax} data={this.props.data}/>
         <div>
           <TextField
             label="Min Limit"
@@ -292,7 +97,28 @@ class Diabetes extends PureComponent {
             color="primary">
               Apply
           </Button>
-          <p style={{color: '#333333', marginTop:10}}>*Negative value are disabled</p>
+          <p style={{color: '#333333', marginTop:10}}>*Les valeurs négatives désactivent les limites</p>
+          <div>
+            <h3>Detaillé</h3>
+            {this.props.data.map(e =>
+              <ExpansionPanel key={e.note.id} style={e.note.anomali_status === 'anomali' ? ({backgroundColor: '#ffe5d0'}) : ({})}>
+                <ExpansionPanelSummary
+                  expandIcon={<ExpandMoreIcon />}
+                >
+                  <Typography>{new Date(e.note.date).toLocaleString()}</Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                  <Typography>
+                    <b>Description: </b>{e.note.data.description || ''}<br/>
+                    <b>Repas: </b>{e.note.data.wichLunch || 'N/A'}<br/>
+                    <b>Glucose: </b>{e.note.data.bloodGlucose || 'N/A'}<br/>
+                    <b>Correction: </b>{e.note.data.insulineCorr || 'N/A'}<br/>
+                    <b>Insuline après repas: </b>{e.note.data.insulineFood || 'N/A'}<br/>
+                  </Typography>
+                </ExpansionPanelDetails>
+              </ExpansionPanel>
+            )}
+          </div>
         </div>
       </div>
     );
@@ -308,65 +134,3 @@ Diabetes.propTypes = {
 };
 
 export default Diabetes;
-
-/*
-{
-      type: 'line',
-      plugins: [ChartAnnotation],
-      data: {
-        labels: table.map(e => e.date.toLocaleString()),
-        datasets: [{
-          label: 'Glycemie',
-          data: table.map(e => ({
-            x: e.date.getTime(),
-            y: e.data,
-        },
-        scales: {
-          xAxes: [{
-            display: true,
-            scaleLabel: {
-              display: true,
-              labelString: 'Dates'
-            }
-          }],
-          yAxes: [{
-            display: true,
-            scaleLabel: {
-              display: true,
-              labelString: 'Taux de glycémie (g/L)'
-            }
-          }]
-        },
-        annotation: {
-          annotations: [{
-            type: 'line',
-            mode: 'horizontal',
-            scaleID: 'y-axis-0',
-            value: '110',
-            borderColor: 'tomato',
-            borderWidth: 1,
-            label: {
-              backgroundColor: 'red',
-              content: 'Hyperglycémie à jeun',
-              enabled: true,
-              position : 'left'
-            }
-          }, {
-            type: 'line',
-            mode: 'horizontal',
-            scaleID: 'y-axis-0',
-            value: '140',
-            borderColor: 'tomato',
-            borderWidth: 1,
-            label: {
-              backgroundColor: 'red',
-              content: 'Hyperglycémie après repas',
-              enabled: table.some(e => e.data > 140),
-              position : 'left'
-            }
-          }],
-          drawTime: 'afterDraw'
-        }
-      }
-    }
-*/
